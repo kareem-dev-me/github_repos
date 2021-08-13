2160368293cc7ae79355e67754a0a980cdb8fa7c
Kareem basuony <kareembasuonyg@gmail.com>
2021-08-13 15:10:04 +0200

    Handling API

--- components/repo-card.vue
+++ components/repo-card.vue
@@ -1,40 +1,38 @@
 <template>
-  <v-card>
+  <v-card class="mx-auto my-5">
     <v-row align="center">
       <!-- Section-> image column -->
       <v-col cols="12" md="3">
-        <v-img
-          src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
-          alt="avatar"
-          max-height="150"
-          class="ma-5"
-        ></v-img>
+        <a :href="owner_url" class="text-decoration-none ma-5" target="_blank">
+          <v-img :src="image" alt="avatar" max-height="150" contain></v-img>
+        </a>
       </v-col>
       <!-- Section-> data column -->
       <v-col cols="12" md="9">
         <!-- Section-> first row -->
-        <p class="text-h5 font-weight-bold">Repo name</p>
+        <p class="text-h5 font-weight-bold">
+          <a :href="repo_url" class="text-decoration-none" target="_blank">
+            {{ name }}
+          </a>
+        </p>
         <!-- Section-> second row -->
 
         <p class="text-body-1">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-          magnam veniam assumenda consequuntur expedita, placeat iure explicabo
-          deleniti libero reprehenderit facere nisi, sunt quasi. Dolor sequi
-          modi repellat deserunt distinctio.
+          {{ description }}
         </p>
         <!-- Section-> third row -->
 
         <div>
           <v-btn color="primary lighten-1" class="mr-2">
             <v-icon color="yellow darken-2" class="mr-2">mdi-star</v-icon>
-            Stars: 118k
+            Stars: {{ stars }}
           </v-btn>
           <v-btn color="primary lighten-1" class="mr-2">
             <v-icon color="red darken-2" class="mr-2">mdi-alert</v-icon>
-            Issues: 1.6k
+            Issues: {{ issues }}
           </v-btn>
           <span class="text-body-2 font-weight-bold">
-            Submitted 30 days ago by test
+            Submitted {{ created }} days ago by {{ owner_name }}
           </span>
         </div>
       </v-col>
@@ -43,7 +41,46 @@
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    image: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      default: '',
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+    stars: {
+      type: Number,
+      default: 0,
+    },
+    issues: {
+      type: Number,
+      default: 0,
+    },
+    owner_url: {
+      type: String,
+      default: '',
+    },
+    owner_name: {
+      type: String,
+      default: '',
+    },
+    repo_url: {
+      type: String,
+      default: '',
+    },
+    created: {
+      type: Number,
+      default: 0,
+    },
+  },
+}
 </script>
 
 <style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
     <v-container>
-      <repo-card></repo-card>
+      <repo-card
+        v-for="item in repos"
+        :key="item.id"
+        :name="item.name"
+        :description="item.description"
+        :image="item.image"
+        :owner_url="item.owner_url"
+        :repo_url="item.repo_url"
+        :stars="item.stars"
+        :issues="item.issues"
+        :owner_name="item.owner_name"
+        :created="item.created"
+      ></repo-card>
     </v-container>
   </div>
 </template>
@@ -12,10 +24,48 @@ export default {
   components: {
     'repo-card': repo_card,
   },
+  data() {
+    return {
+      repos: [],
+    }
+  },
   head() {
     return {
       title: 'Most starred Github repos',
     }
+  },
+  mounted() {
+    // Comment-> 1/ Call the api
+    this.$axios
+      .$get(
+        'https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc'
+      )
+      .then((res) => {
+        // Comment-> 2/ Handle the response and map the arrays
+        this.repos = res.items.map((item) => ({
+          id: item.id,
+          name: item.name,
+          description: item.description,
+          image: item.owner.avatar_url,
+          owner_url: item.owner.html_url,
+          repo_url: item.html_url,
+          stars: item.stargazers_count,
+          issues: item.open_issues_count,
+          owner_name: item.owner.login,
+          created: this.get_days_between_dates(
+            Date.now(),
+            new Date(item.created_at)
+          ),
+        }))
+      })
+    // Comment-> 3/ Dynamic the UI --->
+    // Comment-> 3/ Pagination
+  },
+  methods: {
+    get_days_between_dates(first_date, second_date) {
+      // Comment-> get the difference and divide it by milliseconds in the day
+      return Math.floor((first_date - second_date) / (1000 * 60 * 60 * 24))
+    },
   },
 }
 </script>
